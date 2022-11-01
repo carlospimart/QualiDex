6de73d4570e15e2ad5687873c36564aebd3c1717
sram10 <ramkumar.raja@qualitestgroup.com>
2022-11-01 14:56:35 +0000

    code merge

--- src/main/java/com/qualitestgroup/dataextract/library/QualidexLibrary.java
+++ src/main/java/com/qualitestgroup/dataextract/library/QualidexLibrary.java
@@ -12,6 +12,7 @@ import org.apache.pdfbox.pdmodel.PDResources;
 import org.apache.pdfbox.pdmodel.graphics.PDXObject;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
